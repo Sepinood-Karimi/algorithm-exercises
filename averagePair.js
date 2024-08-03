4f3aa9ba43e7c4e7860919f5617bf25c065f97e5
Sepinoud Karimi <sepinoodkarimi@gmail.com>
2024-08-04 00:03:04 +0330

    feat: implement averagePair

--- averagePair.js
+++ averagePair.js
@@ -13,6 +13,23 @@ averagePair ([-1,0,3,4,5,6], 4.1) // false
 averagePair([1,4) // false
 **/
 
-function averagePair(){
-  // add whatever parameters you deem necessary - good luck!
+const averagePair = (sortedArray,targetAverage) => {
+    let left=0;
+    let right = sortedArray.length -1;
+    while(left<right){
+        let average = (sortedArray[left] + sortedArray[right])/2;
+        if (average===targetAverage){
+            return true;
+        }else if (average<targetAverage){
+            left++;
+        }else{
+            right--;
+        }
+    }
+    return false;
 }
+console.log(averagePair([1,2,31],1.5));
+console.log(averagePair([1,3,3,5,6,7,10,12,191],8));
+console.log(averagePair([-1,0,3,4,5,6],4.1));
+console.log(averagePair([1],4));
+
