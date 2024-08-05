7265ef9a960fd3db9b5b4415a34cd9ac6f6d88ca
Sepinoud Karimi <sepinoodkarimi@gmail.com>
2024-08-06 01:48:03 +0330

    implement maxSubarraySum

--- maxSubarraySum.js
+++ maxSubarraySum.js
@@ -5,17 +5,42 @@ Given an array of integers and a number, write a function called maxSubarraySum,
 
 Note that a subarray must consist of consecutive elements from the original array. In the first example below, [100, 200, 300] is a subarray of the original array, but [100, 300] is not.
 
-maxsubarraySum([100,200,300,4001, 2) // 700
-maxSubarraySum([1,4,2,10,23,3,1,0,201, 4) // 39
-maxSubarraySum([-3,4,0,-2,6, -11, 2) // 5
-maxSubarraySum([3,-2,7,-4,1,-1,4,-2,11,2) // 5
-maxSubarraySum([2,31, 3) // null
+maxsubarraySum([100,200,300,400], 2) // 700
+maxSubarraySum([1,4,2,10,23,3,1,0,20], 4) // 39
+maxSubarraySum([-3,4,0,-2,6, -1], 2) // 5
+maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1],2) // 5
+maxSubarraySum([2,31] ,3) // null
 
 Constraints:
 Time Complexity - O(N)
 Space Complexity - 0(1)
 **/
 
-function maxSubarraySum(){
-  // add whatever parameters you deem necessary - good luck!
+const  maxSubarraySum = (array,lengthOfSum) => {
+    let maxSum =0;
+    let tempSum=0;
+
+    if(array.length<lengthOfSum) return "The length of your some is more than the array length";
+
+    for(let i=0;i<lengthOfSum;i++){
+        maxSum+=array[i];
+    }
+    tempSum = maxSum;
+
+    for (let i=lengthOfSum;i<array.length;i++){
+        tempSum = tempSum - array[i-lengthOfSum] + array[i];
+        maxSum = Math.max(maxSum,tempSum);
+    }
+    return maxSum;
 }
+
+console.log(maxSubarraySum([2,31] ,3));
+console.log(maxSubarraySum([100,200,300,400] ,2));
+console.log(maxSubarraySum([1,4,2,10,23,3,1,0,20], 4));
+console.log(maxSubarraySum([-3,4,0,-2,6, -1], 2));
+console.log(maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1],2));
+console.log(maxSubarraySum([1,2,5,2,8,1,5],2));
+console.log(maxSubarraySum([1,2,5,2,8,1,5],4));
+console.log(maxSubarraySum([4,2,1,6],1));
+console.log(maxSubarraySum([4,2,1,6],4));
+console.log(maxSubarraySum([],4));
