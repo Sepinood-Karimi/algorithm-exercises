caa11035f156b3de505c762d3da73a8312573b73
Sepinoud Karimi <sepinoodkarimi@gmail.com>
2024-08-06 23:26:13 +0330

    implement DoublyLinkedList

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -3,40 +3,124 @@ Complete the following DoublyLinkedList
 **/
 
 
-class Node{
-    constructor(val){
+class Node {
+    constructor(val) {
         this.val = val;
         this.next = null;
         this.prev = null;
     }
 }
 
-
 class DoublyLinkedList {
-    constructor(){
+    constructor() {
         this.head = null;
         this.tail = null;
         this.length = 0;
     }
-    push(val){
-        // code here
-    } 
-    pop(){
-        // code here
+
+    push(val) {
+        const newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
     }
-    shift(){
-        // code here
+
+    pop() {
+        if (!this.head) return undefined;
+        const poppedNode = this.tail;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = poppedNode.prev;
+            this.tail.next = null;
+            poppedNode.prev = null;
+        }
+        this.length--;
+        return poppedNode;
     }
-    unshift(val){
-        // code here
+
+    shift() {
+        if (!this.head) return undefined;
+        const oldHead = this.head;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = oldHead.next;
+            this.head.prev = null;
+            oldHead.next = null;
+        }
+        this.length--;
+        return oldHead;
     }
-    get(index){
-        // code here
+
+    unshift(val) {
+        const newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head.prev = newNode;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
     }
-    set(index, val){
-      // code here
+
+    get(index) {
+        if (index < 0 || index >= this.length) return null;
+        let count, current;
+        if (index <= this.length / 2) {
+            count = 0;
+            current = this.head;
+            while (count !== index) {
+                current = current.next;
+                count++;
+            }
+        } else {
+            count = this.length - 1;
+            current = this.tail;
+            while (count !== index) {
+                current = current.prev;
+                count--;
+            }
+        }
+        return current;
     }
-    insert(index, val){
-      // code here
+
+    set(index, val) {
+        const foundNode = this.get(index);
+        if (foundNode != null) {
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
+
+    insert(index, val) {
+        if (index < 0 || index > this.length) return false;
+        if (index === 0) return !!this.unshift(val);
+        if (index === this.length) return !!this.push(val);
+
+        const newNode = new Node(val);
+        const beforeNode = this.get(index - 1);
+        const afterNode = beforeNode.next;
+
+        beforeNode.next = newNode;
+        newNode.prev = beforeNode;
+        newNode.next = afterNode;
+        if (afterNode != null) afterNode.prev = newNode;
+        this.length++;
+        return true;
     }
 }
+
