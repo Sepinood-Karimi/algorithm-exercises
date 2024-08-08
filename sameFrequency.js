a136f796140ac847c7c9c31dd6054691a903c0cb
Sepinoud Karimi <sepinoodkarimi@gmail.com>
2024-08-08 09:58:06 +0330

    fix: add null and undefined edge case

--- sameFrequency.js
+++ sameFrequency.js
@@ -13,17 +13,18 @@ sameFrequency (22,222) // false
 
 const sameFrequency = (number1,number2) =>{
     if (number1<0 || number2<0) return "enter positive int";
+    if (number1===null||undefined || number2===null||undefined) return "please enter numbers"
     const stringNumber1 = number1.toString();
     const stringNumber2 = number2.toString();
     if (stringNumber1.length!==stringNumber2.length) return false;
 
     const frequencyCounter1 = {};
     for (let number of stringNumber1){
-        frequencyCounter1[number] = (frequencyCounter1[number] || 0) + 1;
+        frequencyCounter1[number] = (frequencyCounter1[number] ?? 0) + 1;
     }
     const frequencyCounter2 = {};
     for (let number of stringNumber2){
-        frequencyCounter2[number] = (frequencyCounter2[number] || 0) + 1;
+        frequencyCounter2[number] = (frequencyCounter2[number] ?? 0) + 1;
     }
     for (let count in frequencyCounter1) {
         if (frequencyCounter1[count] !== frequencyCounter2[count]) return false;
@@ -38,4 +39,6 @@ console.log(sameFrequency (22,222));
 console.log(sameFrequency (182, 281));
 console.log(sameFrequency (34,14));
 console.log(sameFrequency (3589578, 5879385));
+console.log(sameFrequency(null,null))
+console.log(sameFrequency(undefined,null))
 
